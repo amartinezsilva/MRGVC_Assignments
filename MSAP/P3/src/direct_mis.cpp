f09058a25950a20d379ff71487be4f2779d32801
amartinezsilva <andresmarsil77@gmail.com>
2023-11-12 19:36:00 +0100

    exercise 3 cleaned

--- MSAP/P3/src/direct_mis.cpp
+++ MSAP/P3/src/direct_mis.cpp
@@ -37,49 +37,11 @@ public:
 		
 		// -------------------------------- MIS PART (FILL HERE) -------------------------------------------------------//
 
-		float em_pdf, mat_pdf;
+		float em_pdf = 0.0f, mat_pdf;
 		float w_em = 0.0f, w_mat = 0.0f;
 
-		// ----------- F_ems part -------------------//
-		Color3f L_em(0.0f);
-		//Sample emitter
-		EmitterQueryRecord emitterRecord_ems(its.p);
-		float pdflight, pdfpositionlight;
-		const Emitter *random_emitter = scene->sampleEmitter(sampler->next1D(), pdflight);
-		Color3f Li_em = random_emitter->sample(emitterRecord_ems, sampler->next2D(), 0.);
-		pdfpositionlight = random_emitter->pdf(emitterRecord_ems);
-		
-		em_pdf = pdflight * pdfpositionlight; // ??? //
-
-		float cos_theta_i = Frame::cosTheta(its.shFrame.toLocal(emitterRecord_ems.wi));
-
-		// Visibility query
-		Ray3f shadow_ray(its.p, emitterRecord_ems.wi);
-		// Ensures that the shadow ray does not overshoot the light source.
-		shadow_ray.maxt = (emitterRecord_ems.p - its.p).norm();
-		Intersection shadowIntersection;
-		bool shadow_intersects = scene->rayIntersect(shadow_ray, shadowIntersection);
-		if (shadow_intersects){
-			L_em = Color3f(0.0f);
-		}
-		else{
-			//Evaluate pdf of material sampling using a sample generated with emitter sampling
-			BSDFQueryRecord bsdfRecord_ems(its.toLocal(-ray.d), its.toLocal(emitterRecord_ems.wi), its.uv, ESolidAngle);
-			mat_pdf = its.mesh->getBSDF()->pdf(bsdfRecord_ems);
-			Color3f f = its.mesh->getBSDF()->eval(bsdfRecord_ems);
-			// Weights
-			w_em = em_pdf / (em_pdf + mat_pdf);
-			//Emitter mat
-			Color3f F_ems = Li_em * f * cos_theta_i / em_pdf;
-			L_em = w_em * F_ems;
-		}
-
-		Lo += L_em;
-
-
 		// ----------- F_mat part -------------------//
-
-		Color3f L_mat(0.0f);
+		Color3f F_mat(0.0f);
 		//Sample BSDF
 		BSDFQueryRecord bsdfRecord_mat(its.toLocal(-ray.d), its.uv);
 		Color3f throughput = its.mesh->getBSDF()->sample(bsdfRecord_mat, sampler->next2D());
@@ -103,92 +65,52 @@ public:
 			Le_r = new_its.mesh->getEmitter()->eval(new_emitterRecord);
 			//Evaluate pdf of emitter sampling using a sample generated with material sampling
 			em_pdf = new_its.mesh->getEmitter()->pdf(new_emitterRecord);
-			// Weights
-			w_mat = mat_pdf / (em_pdf + mat_pdf);
 		}
-		//Emitter mat
-		Color3f F_mat = Le_r * throughput;
-	    L_mat = w_mat * F_mat;
+
+		F_mat = Le_r * throughput;
+		// Weights material sampling part
+		if(mat_pdf + em_pdf > 0.0f) w_mat = mat_pdf / (mat_pdf + em_pdf);
+		else w_mat = mat_pdf;
+
+		Color3f L_mat = w_mat * F_mat;
 		Lo += L_mat;
 
-		// // -------------------------------- EMS PART -------------------------------------------------------//
-
-		// EmitterQueryRecord emitterRecord(its.p);
-
-		// float pdflight;
-		// // Sample random light source
-		// const Emitter *random_emitter = scene->sampleEmitter(sampler->next1D(), pdflight);
-
-		// Color3f Le = random_emitter->sample(emitterRecord, sampler->next2D(), 0.);
-
-		// // Check if intersected material is emitter
-		// if(its.mesh->isEmitter()){
-		// 	emitterRecord.ref = ray.o;
-		// 	emitterRecord.wi = -ray.d;
-		// 	emitterRecord.n = its.shFrame.n;
-		// 	return its.mesh->getEmitter()->sample(emitterRecord, sampler->next2D(), 0.);
-		// }
-
+		// ----------- F_ems part -------------------//
+		mat_pdf = 0.0f;
+		Color3f F_ems(0.0f);
+		//Sample emitter
+		EmitterQueryRecord emitterRecord_ems(its.p);
+		float pdflight, pdfpositionlight;
+		const Emitter *random_emitter = scene->sampleEmitter(sampler->next1D(), pdflight);
+		Color3f Li_em = random_emitter->sample(emitterRecord_ems, sampler->next2D(), 0.);
+		pdfpositionlight = random_emitter->pdf(emitterRecord_ems);
 		
-		// // Here perform a visibility query, to check whether the light 
-		// // source "em" is visible from the intersection point. 
-		// // For that, we create a ray object (shadow ray),
-		// // and compute the intersection
-		
-		// Ray3f shadow_ray(its.p, emitterRecord.wi);
-		// // Ensures that the shadow ray does not overshoot the light source.
-		// shadow_ray.maxt = (emitterRecord.p - its.p).norm();
-		// Intersection shadowIntersection;
-		// if (scene->rayIntersect(shadow_ray, shadowIntersection)){
-		// 	return Lo;
-		// }
+		em_pdf = pdflight * pdfpositionlight; // ??? //
 
-		// float pdfpositionlight = random_emitter->pdf(emitterRecord);
- 
-		// BSDFQueryRecord bsdfRecord(its.toLocal(-ray.d),its.toLocal(emitterRecord.wi), its.uv, ESolidAngle);
-        // Color3f fr = its.mesh->getBSDF()->eval(bsdfRecord);
-        // float cos_theta_i = its.shFrame.n.dot(emitterRecord.wi);	
+		float cos_theta_i = its.shFrame.n.dot(emitterRecord_ems.wi);
 
-		// // For each light, we accomulate the incident light times the 
-		// // foreshortening times the BSDF term (i.e. the render equation). 
-		// Lo += Le * fr * cos_theta_i / (pdflight*pdfpositionlight);
+		// Visibility query
+		Ray3f shadow_ray(its.p, emitterRecord_ems.wi);
+		// Ensures that the shadow ray does not overshoot the light source.
+		shadow_ray.maxt = (emitterRecord_ems.p - its.p).norm();
+		Intersection shadowIntersection;
+		bool shadow_intersects = scene->rayIntersect(shadow_ray, shadowIntersection);
+		if(!shadow_intersects){		
+			//Evaluate pdf of material sampling using a sample generated with emitter sampling
+			BSDFQueryRecord bsdfRecord_ems(its.toLocal(-ray.d), its.toLocal(emitterRecord_ems.wi), its.uv, ESolidAngle);
+			mat_pdf = its.mesh->getBSDF()->pdf(bsdfRecord_ems);
+			Color3f f = its.mesh->getBSDF()->eval(bsdfRecord_ems);
+			//Emitter mat
+			F_ems = Li_em * f * cos_theta_i / em_pdf;
+		}
 
-		// // ------------------------  MATS PART -----------------------------------------//
-		
-		// //EMITTER
-		// if(its.mesh->isEmitter()){
-		// 	EmitterQueryRecord emitterRecord;
-		// 	emitterRecord.wi = ray.d;
-		// 	emitterRecord.n = its.shFrame.n;
-		// 	return its.mesh->getEmitter()->eval(emitterRecord);
-		// }
+		// Weights material sampling part
+		if(mat_pdf + em_pdf > 0.0f) w_em = em_pdf / (mat_pdf + em_pdf);
+		else w_em = em_pdf;
 
+		Color3f L_em = w_em * F_ems;
 
-		// //BSDF
-		// BSDFQueryRecord bsdfRecord(its.toLocal(-ray.d), its.uv);
-		// Color3f throughput = its.mesh->getBSDF()->sample(bsdfRecord, sampler->next2D());
-
-		// Vector3f new_dir = its.toWorld(bsdfRecord.wo);
-
-		// //reflect
-        // Ray3f rayR = Ray3f(its.p, new_dir);
-
-		// Intersection new_its;
-		// Color3f Le_r(0.0f);
-
-		// bool intersection = scene->rayIntersect(rayR, new_its);
-		// if (!intersection) {
-		// 	Le_r = scene->getBackground(rayR);
-		// } else if(new_its.mesh->isEmitter()) {
-		// 	//std::cout <<"emitterrr" << endl;
-		// 	EmitterQueryRecord new_emitterRecord;
-		// 	new_emitterRecord.wi = rayR.d;
-		// 	new_emitterRecord.n = new_its.shFrame.n;
-		// 	Le_r = new_its.mesh->getEmitter()->eval(new_emitterRecord);	
-		// }
-			
-		// Lo += Le_r * throughput;
-
+		Lo += L_em;
 
 		return Lo;
 	}
