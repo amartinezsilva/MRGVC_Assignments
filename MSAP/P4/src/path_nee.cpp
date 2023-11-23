7d82d80b0ac33273dbf87995ea7e057565843ee4
amartinezsilva <andresmarsil77@gmail.com>
2023-11-23 23:53:27 +0100

    corrected exercise 2 - i think

--- MSAP/P4/src/path_nee.cpp
+++ MSAP/P4/src/path_nee.cpp
@@ -18,6 +18,7 @@ public:
 
 	Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray) const {
 		Color3f Lo(0.);
+		Color3f Le_acc(0.);
 
 		// Find the surface that is visible in the requested direction 
 		Intersection its;
@@ -59,12 +60,13 @@ public:
 			BSDFQueryRecord bsdfRecord(its.toLocal(-ray.d),its.toLocal(emitterRecord.wi), its.uv, ESolidAngle);
 			Color3f fr = its.mesh->getBSDF()->eval(bsdfRecord);
 			float cos_theta_i = its.shFrame.n.dot(emitterRecord.wi);	
-			Lo += Le * fr * cos_theta_i / (pdflight*pdfpositionlight);
+			Le_acc += Le * fr * cos_theta_i / (pdflight*pdfpositionlight);
 		}
 
 		//BSDF
 		BSDFQueryRecord bsdfRecord_samp(its.toLocal(-ray.d), its.uv);
 		Color3f throughput = its.mesh->getBSDF()->sample(bsdfRecord_samp, sampler->next2D());
+		Lo += throughput;
 
 
 		Vector3f new_dir = its.toWorld(bsdfRecord_samp.wo);
@@ -73,13 +75,12 @@ public:
         Ray3f rayR = Ray3f(its.p, new_dir);
 
 		Intersection new_its;
-		Color3f Le_r(0.0f);
 		bool intersection = scene->rayIntersect(rayR, new_its);
-		if(intersection && new_its.mesh->isEmitter()) {	
+		if(intersection && (new_its.mesh->isEmitter() && bsdfRecord_samp.measure != EDiscrete)) {		
 			return Lo * Li(scene, sampler, rayR) / 0.8;
 		}
 
-		Lo += throughput;
+		Lo += Le_acc;
 
 		return Lo * Li(scene, sampler, rayR) / 0.8;
 
