0ed5832f3088db6d225675c5b1d320a6092114ff
amartinezsilva <andresmarsil77@gmail.com>
2023-12-03 10:40:45 +0100

    draw points

--- CV/labSession5/points_3D_reconstruction.py
+++ CV/labSession5/points_3D_reconstruction.py
@@ -21,6 +21,46 @@ import scipy.optimize as scOptim
 import cv2
 
 
+def visualize_2D_points(image, real_points, estimated_points):
+    """
+    Visualize real and estimated 2D points on an image.
+
+    Parameters:
+    - image: The image to display.
+    - real_points: A 2xN array representing the real 2D points.
+    - estimated_points: A 2xN array representing the estimated 2D points.
+    """
+
+    # Create a figure and axis
+    fig, ax = plt.subplots()
+
+    # Display the image
+    ax.imshow(image)
+
+    # Plot real points with red arrows
+    ax.quiver(real_points[0], real_points[1], np.zeros_like(real_points[0]), np.zeros_like(real_points[1]),
+              angles='xy', scale_units='xy', scale=1, color='red', label='Real Points')
+
+    # Plot estimated points with blue arrows
+    ax.quiver(estimated_points[0], estimated_points[1], np.zeros_like(estimated_points[0]), np.zeros_like(estimated_points[1]),
+              angles='xy', scale_units='xy', scale=1, color='blue', label='Estimated Points')
+
+    # Plot lines connecting corresponding points
+    for real_point, estimated_point in zip(real_points.T, estimated_points.T):
+        ax.plot([real_point[0], estimated_point[0]], [real_point[1], estimated_point[1]], 'black', linestyle='dashed')
+
+    # Set axis limits
+    ax.set_xlim([0, image.shape[1]])
+    ax.set_ylim([image.shape[0], 0])  # y-axis is inverted in images
+
+    # Add legend
+    ax.legend()
+
+    # Show the plot
+    plt.show()
+
+
+
 def kannala_projection(K_c, X, D):
     
     [k1,k2,k3,k4] = D[0:4]
@@ -28,29 +68,29 @@ def kannala_projection(K_c, X, D):
     R = np.sqrt(pow(X[0,:],2) + pow(X[1,:],2))
     theta = np.arctan2(R, X[2,:])
     d = theta + k1*pow(theta,3) + k2 * pow(theta, 5) + k3 * pow(theta, 7) + k4 * pow(theta, 9)
-    u = np.dot(K_c, np.array([d*np.cos(phi), d*np.sin(phi), [1]]))
+    u = K_c @ np.array([d*np.cos(phi), d*np.sin(phi), [1]])
     return u
 
 def kannala_unprojection(K_c, u, D):
 
     [k1,k2,k3,k4] = D[0:4]
-    point_c = np.dot(np.linalg.inv(K_c), u)
-    phi = np.arctan2(point_c[1,:], point_c[0,:])
-    num = pow(point_c[0,:], 2) + pow(point_c[1,:], 2)
-    den = pow(point_c[2,:], 2)
+    point_c = np.linalg.inv(K_c) @ u
+    phi = np.arctan2(point_c[1,0], point_c[0,0])
+    num = pow(point_c[0,0], 2) + pow(point_c[1,0], 2)
+    den = pow(point_c[2,0], 2)
     r = np.sqrt(num / den)
     theta = np.roots([k4, 0, k3, 0, k2, 0, k1, 0, 1, -r])
-    theta = theta[np.isreal(theta)]
+    theta = float(np.real(theta[np.isreal(theta)]))
 
     v = np.array([[np.sin(theta) * np.cos(phi)], [np.sin(theta) * np.sin(phi)], [np.cos(phi)]]).reshape(3,1)
 
-    return np.real(v)
+    return v
 
 def calculate_planes_from_v(v):
 
-    vx = float(v[0,0])
-    vy = float(v[1,0])
-    vz = float(v[2,0])
+    vx = v[0,0]
+    vy = v[1,0]
+    vz = v[2,0]
 
     pi_sym = np.array([[-vy], [vx], [0], [0]]).reshape(4,1)
     pi_perp = np.array([[-vz*vx], [-vz*vy], [pow(vx,2) + pow(vy, 2)], [0]]).reshape(4,1)
@@ -114,14 +154,16 @@ if __name__ == '__main__':
 
     print("Unprojection with C1:")
 
-    v1_c1 = kannala_unprojection(K_1, u1, D1_k_array)
-    v2_c1 = kannala_unprojection(K_1, u2, D1_k_array)
-    v3_c1 = kannala_unprojection(K_1, u3, D1_k_array)
+    v1_c1 = kannala_unprojection(K_1, u1_c1, D1_k_array)
+    print(v1_c1)
+    v2_c1 = kannala_unprojection(K_1, u2_c1, D1_k_array)
+    v3_c1 = kannala_unprojection(K_1, u3_c1, D1_k_array)
 
     print("Unprojection with C2:")
-    v1_c2 = kannala_unprojection(K_2, u1, D2_k_array)
-    v2_c2 = kannala_unprojection(K_2, u2, D2_k_array)
-    v3_c2 = kannala_unprojection(K_2, u3, D2_k_array)
+    v1_c2 = kannala_unprojection(K_2, u1_c2, D2_k_array)
+    print(v1_c2)
+    v2_c2 = kannala_unprojection(K_2, u2_c2, D2_k_array)
+    v3_c2 = kannala_unprojection(K_2, u3_c2, D2_k_array)
     
     ##3.2 Triangulation for pose A
     
@@ -132,27 +174,25 @@ if __name__ == '__main__':
     X_triangulated_c2 = np.zeros((4, npoints))
     for i in range(npoints):
 
-        u1_c1 = x1Data[:,i].reshape(3,1)
-        u1_c2 = x2Data[:,i].reshape(3,1)
+        u1 = x1Data[:,i].reshape(3,1)
+        u2 = x2Data[:,i].reshape(3,1)   
 
-        v1_c1 = kannala_unprojection(K_1, u1_c1, D1_k_array)
-        v1_c2 = kannala_unprojection(K_2, u1_c2, D2_k_array)
+        v1 = kannala_unprojection(K_1, u1, D1_k_array)
+        v2 = kannala_unprojection(K_2, u2, D2_k_array)
         
         #Triangulation
-        pi_sym1, pi_perp1 = calculate_planes_from_v(v1_c1)
-        pi_sym2, pi_perp2 = calculate_planes_from_v(v1_c2)
+        pi_sym1, pi_perp1 = calculate_planes_from_v(v1)
+        pi_sym2, pi_perp2 = calculate_planes_from_v(v2)
 
         pi_sym1_c2 = np.dot(T_leftright.T, pi_sym1)
         pi_perp1_c2 = np.dot(T_leftright.T, pi_perp1)
 
         A = np.zeros((4,4))
-        A[0][:] = pi_sym1_c2.T
-        A[1][:] = pi_perp1_c2.T
-        A[2][:] = pi_sym2.T
-        A[3][:] = pi_perp2.T
+        A[0,:] = pi_sym1_c2.T
+        A[1,:] = pi_perp1_c2.T
+        A[2,:] = pi_sym2.T
+        A[3,:] = pi_perp2.T
 
-        print("Triangulation matrix A:")
-        print(A)
         u, s, vh = np.linalg.svd(A)
         X = np.reshape(vh[-1, :],(4,1))
 
@@ -164,43 +204,36 @@ if __name__ == '__main__':
         X_triangulated_c2[3][i] = X[3,:]
 
 
-    print(X_triangulated_c2)
     X_triangulated_w = np.dot(T_wc2, X_triangulated_c2)
-    X_triangulated_c1 = np.dot(np.linalg.inv(T_wc1), X_triangulated_w)
-
-    #Check with projection model with one point
-
-    i = 10
-    X1_c1 = X_triangulated_c1[:,i].reshape(4,1)
-    X1_c2 = X_triangulated_c2[:,i].reshape(4,1)
-    u1_c1 = kannala_projection(K_1, X1_c1, D1_k_array)
-    print("Projection triangulated first point in C1")
-    print(u1_c1)
-    print("Projection triangulated first point in C2")
-    u1_c2 = kannala_projection(K_2, X1_c2, D2_k_array)
-    print(u1_c2)
-
+    X_triangulated_c1 = np.dot(T_leftright, X_triangulated_c2)
+    #X_triangulated_c1 = np.dot(np.linalg.inv(T_wc1), X_triangulated_w)
 
     #Check with projection model for all points
 
-    u_c1 = np.zeros((3,npoints))
-    u_c2 = np.zeros((3,npoints))
+    u_1 = np.zeros((3,npoints))
+    u_2 = np.zeros((3,npoints))
 
     for i in range(npoints):
+
         X_c1 = X_triangulated_c1[:,i].reshape(4,1)
         X_c2 = X_triangulated_c2[:,i].reshape(4,1)
+        X_w = X_triangulated_w[:,i].reshape(4,1)
 
-        u_c1_p = kannala_projection(K_1, X_c1, D1_k_array)
+        u_1_p = kannala_projection(K_1, X_c1, D1_k_array)
 
-        u_c1[0][i] = u_c1_p[0,:]
-        u_c1[1][i] = u_c1_p[1,:]
-        u_c1[2][i] = u_c1_p[2,:]
+        u_1[0,i] = u_1_p[0,:]
+        u_1[1,i] = u_1_p[1,:]
+        u_1[2,i] = u_1_p[2,:]
 
-        u_c2_p = kannala_projection(K_2, X_c2, D2_k_array)
+        u_2_p = kannala_projection(K_2, X_c2, D2_k_array)
 
-        u_c2[0][i] = u_c2_p[0,:]
-        u_c2[1][i] = u_c2_p[1,:]
-        u_c2[2][i] = u_c2_p[2,:]
+        u_2[0,i] = u_2_p[0,:]
+        u_2[1,i] = u_2_p[1,:]
+        u_2[2,i] = u_2_p[2,:]
+
     
-    np.savetxt('x1Data_tri.txt', u_c1)
-    np.savetxt('x2Data_tri.txt', u_c2)
+    np.savetxt('x1Data_tri.txt', u_1)
+    np.savetxt('x2Data_tri.txt', u_2)
+
+    visualize_2D_points(fisheye1_frameA, x1Data, u_1)
+    visualize_2D_points(fisheye2_frameA, x2Data, u_2)
