44b6580d618f7b27efe6d72a1c9530141833ebeb
amartinezsilva <andresmarsil77@gmail.com>
2023-12-30 17:56:16 +0100

    medium class updated

--- MSAP/project/include/nori/medium.h
+++ MSAP/project/include/nori/medium.h
@@ -2,62 +2,59 @@
 
 #include <nori/object.h>
 #include <nori/bbox.h>
-#include <nori/phaseFunction.h>
+#include <nori/phase_function.h>
 #include <nori/sampler.h>
 
 NORI_NAMESPACE_BEGIN
 
 class Medium;
 
-struct MediumSamplingRecord {
-    /// Traveled distance
+
+
+struct MediumInterface {
+    // Inside and outside medium
+    Medium *inside, *outside;
+
+    // When only one medium is provided, both sides are the same
+    MediumInterface(): inside(NULL), outside(NULL) { }
+
+    MediumInterface(Medium *medium)
+        : inside(medium), outside(medium) { }
+    
+    MediumInterface(Medium *inside, Medium *outside)
+        : inside(inside), outside(outside) { }
+
+    bool IsMediumTransition() const { return inside != outside; }
+};
+
+
+struct MediumInteraction {
+    
+    /// Traveled unoccluded distance along the ray
     float t;
 
     /// Location of the scattering interaction
     Point3f p;
 
-    /// Local particle orientation at \ref p
-    Vector3f orientation;
+    // Incident direction
+    Vector3f wi;
+    // Scattered direction
+    Vector3f wo;
 
-    /**
-     * \brief Specifies the transmittance along the segment [mint, t]
-     *
-     * When sampling a distance fails, this contains the
-     * transmittance along the whole ray segment [mint, maxDist].
-     */
-    Color3f transmittance;
-
-    /// The medium's absorption coefficient at \ref p
-    Color3f sigmaA;
-
-    /// The medium's scattering coefficient at \ref p
-    Color3f sigmaS;
-
-    /// Records the probability density of sampling a medium interaction at p
-    float pdfSuccess;
-
-
-    /**
-     * When the \ref Medium::sampleDistance() is successful, this function
-     * returns the probability of \a not having generated a medium interaction
-     * until \ref t. Otherwise, it records the probability of
-     * not generating any interactions in the whole interval [mint, maxt].
-     * This probability is assumed to be symmetric with respect to
-     * sampling from the other direction, which is why there is no
-     * \c pdfFailureRev field.
-     */
-    float pdfFailure;
+    /// Phase function for the medium
+    const PhaseFunction *phase;
 
     /// Pointer to the associated medium
     const Medium *medium;
 
-    inline MediumSamplingRecord() : medium(NULL) { }
+    MediumInteraction() {medium = NULL; phase = NULL;};
+    MediumInteraction(Point3f p_, float t_, Vector3f wi_):
+    p(p_), t(t_), wi(wi_), phase(NULL), medium(NULL) { }
 
-    /// Return a pointer to the phase function
-    inline const PhaseFunction *getPhaseFunction() const;
+    MediumInteraction(Point3f p_, float t_, Vector3f wi_, const PhaseFunction *phase_, const Medium *medium_):
+    p(p_), t(t_), wi(wi_), phase(phase_), medium(medium_) { }
 
-    /// Return a string representation
-    std::string toString() const;
+    bool isValid() const { return phase != NULL; }
 };
 
 //homogeneous medium
@@ -65,98 +62,57 @@ struct MediumSamplingRecord {
 class Medium : public NoriObject {
 public:
 
-    /**
-     * \brief Sample a distance along the ray segment [mint, maxt]
-     *
-     * Should ideally importance sample with respect to the transmittance.
-     * It is assumed that the ray has a normalized direction value.
-     *
-     * \param ray      Ray, along which a distance should be sampled
-     * \param mRec     Medium sampling record to be filled with the result
-     * \return         \c false if the maximum distance was exceeded, or if
-     *                 no interaction inside the medium could be sampled.
-     */
-    virtual bool sampleDistance(const Ray3f &ray,
-        MediumSamplingRecord &mRec, const Point2f &sample) const = 0;
-        
-    /**
-     * \brief Compute the 1D density of sampling distance \a ray.maxt
-     * along the ray using the sampling strategy implemented by
-     * \a sampleDistance.
-     *
-     * The function computes the continuous densities in the case of
-     * a successful \ref sampleDistance() invocation (in both directions),
-     * as well as the Dirac delta density associated with a failure.
-     * For convenience, it also stores the transmittance along the
-     * supplied ray segment within \a mRec.
-     */
-    virtual void eval(const Ray3f &ray,
-    MediumSamplingRecord &mRec) const = 0;
 
-    /**
-     * \brief Compute the transmittance along a ray segment
-     *
-     * Computes the transmittance along a ray segment
-     * [mint, maxt] associated with the ray. It is assumed
-     * that the ray has a normalized direction value.
-     */
-    virtual Color3f evalTransmittance(const Ray3f &ray, const Point2f &sample) const = 0;
+    virtual Color3f Tr(const Ray3f &ray) const = 0;
+    virtual Color3f sample(const Ray3f &ray, const Point2f &sample, MediumInteraction &mRec) const = 0;
 
 
     /// Return the phase function of this medium
     inline const PhaseFunction *getPhaseFunction() const { return m_phaseFunction; }
 
-    /// Determine whether the medium is homogeneous
-    virtual bool isHomogeneous() const = 0;
+    // /// Determine whether the medium is homogeneous
+    // virtual bool isHomogeneous() const = 0;
 
-    /// For homogeneous media: return the absorption coefficient
-    inline const Color3f &getSigmaA() const { return m_sigmaA; }
+    // /// For homogeneous media: return the absorption coefficient
+    // inline const Color3f &getSigmaA() const { return m_sigmaA; }
 
-    /// For homogeneous media: return the scattering coefficient
-    inline const Color3f &getSigmaS() const { return m_sigmaS; }
+    // /// For homogeneous media: return the scattering coefficient
+    // inline const Color3f &getSigmaS() const { return m_sigmaS; }
 
-    /// For homogeneous media: return the extinction coefficient
-    inline const Color3f &getSigmaT() const { return m_sigmaT; }
+    // /// For homogeneous media: return the extinction coefficient
+    // inline const Color3f &getSigmaT() const { return m_sigmaT; }
 
-    /// For homogeneous media: return the albedo coefficient
-    inline const Color3f get_albedo() const { return m_albedo; }
+    // /// For homogeneous media: return the albedo coefficient
+    // inline const Color3f get_albedo() const { return m_albedo; }
 
     /// Add a child
     /// Register a child object (e.g. a BSDF) with the mesh
-    virtual void addChild(NoriObject *child, const std::string& name = "none") {};
+    virtual void addChild(NoriObject *child, const std::string& name = "none") override
+    {
+        switch (child->getClassType()) {
+        case EPhaseFunction:
+            if (m_phaseFunction)
+                throw NoriException(
+                    "Medium: tried to register multiple instances!");
+            m_phaseFunction = static_cast<PhaseFunction *>(child);
+            break;
 
-    /// Return a string representation
-    virtual std::string toString() const = 0;
+        default:
+            throw NoriException("Medium::addChild(<%s>) is not supported!",
+                                classTypeName(child->getClassType()));
+        }
+    }
 
     EClassType getClassType() const { return EMedium; }
 
 protected:
      //Variables
     PhaseFunction* m_phaseFunction = NULL;
-    Color3f m_sigmaA;
-    Color3f m_sigmaS;
-    Color3f m_sigmaT;
-    Color3f m_albedo;
+    // Color3f m_sigmaA;
+    // Color3f m_sigmaS;
+    // Color3f m_sigmaT;
+    // Color3f m_albedo;
 };
-
-// class HomogeneousMedium : public Medium {
-// public:
-//         HomogeneousMedium(const PropertyList &props);
-//         //HomogeneousMedium(PropertyList &props);
-//         Color3f Tr(const Point3f &a, const Point3f &b) const;
-//         Color3f evalTransmittance(const Ray3f &ray, const Point2f &sample) const;
-//         Color3f sample(const Ray3f &ray, Sampler *sampler, MediumInteractionQuery &mi) const;
-
-//         std::string toString() const override;
-//         EClassType  getClassType() const override { return EMedium; }
-
-// private:
-//        const Color3f m_sigma_a, m_sigma_s, m_sigma_t;
-//        const float m_g;
-//        PhaseFunction * m_phaseFunction = nullptr;
-
-// };
-
 
 
 NORI_NAMESPACE_END
--- MSAP/project/src/medium.cpp
+++ MSAP/project/src/medium.cpp
@@ -12,135 +12,38 @@ public:
         m_albedo = m_sigmaS / m_sigmaT;
     }
 
-    bool sampleDistance(const Ray3f &ray, MediumSamplingRecord &mRec, const Point2f &sample) const {
-        float rand = sample(0);
-        float sampledDistance;
-        float samplingDensity;
-
-        // extintion coefficient
-        int channel = std::min((int) (sample(1) * 3.0f), 2);
-        samplingDensity = m_sigmaT[channel];
-
-        sampledDistance = -std::log(1.0f - rand) / samplingDensity;
-
-        float distSurf = ray.maxt - ray.mint;
-        bool success = true;
-        if (sampledDistance < distSurf) {
-            mRec.t = sampledDistance + ray.mint;
-            mRec.p = ray(mRec.t);
-            mRec.sigmaA = m_sigmaA;
-            mRec.sigmaS = m_sigmaS;
-            mRec.medium = this;
-
-            /* Fail if there is no forward progress
-               (e.g. due to roundoff errors) */
-            if (mRec.p == ray.o)
-                success = false;
-        } else {
-            sampledDistance = distSurf;
-            success = false;
-        }
-
-
-        mRec.pdfFailure = 0.0f;
-        mRec.pdfSuccess = 0.0f;
-        for (int i=0; i<3; ++i) {
-            float tmp = std::exp(-m_sigmaT[i] * sampledDistance);
-            mRec.pdfFailure += tmp;
-            mRec.pdfSuccess += m_sigmaT[i] * tmp;
-        }
-        mRec.pdfFailure /= 3.0f;
-        mRec.pdfSuccess /= 3.0f;
-
-        Color3f temp = (m_sigmaT * (-sampledDistance));
-        mRec.transmittance = Color3f(expf(temp.r()), expf(temp.g()), expf(temp.b()));
-
-        mRec.medium = this;
-        if (mRec.transmittance.maxCoeff() < 1e-20)
-            mRec.transmittance = Color3f(0.0f);
-
-        return success;
-    }
-    
-    /**
-     * \brief Compute the 1D density of sampling distance \a ray.maxt
-     * along the ray using the sampling strategy implemented by
-     * \a sampleDistance.
-     *
-     * The function computes the continuous densities in the case of
-     * a successful \ref sampleDistance() invocation (in both directions),
-     * as well as the Dirac delta density associated with a failure.
-     * For convenience, it also stores the transmittance along the
-     * supplied ray segment within \a mRec.
-     */
-    void eval(const Ray3f &ray, MediumSamplingRecord &mRec) const {
-        float distance = ray.maxt - ray.mint;
-
-        mRec.pdfSuccess = 0;
-        mRec.pdfFailure = 0;
-        for (int i=0; i<3; ++i) {
-            float temp = std::exp(-m_sigmaT[i] * distance);
-            mRec.pdfSuccess += m_sigmaT[i] * temp;
-            mRec.pdfFailure += temp;
-        }
-        mRec.pdfSuccess /= 3;
-        mRec.pdfFailure /= 3;
-        Color3f temp = m_sigmaT * (-distance);
-        std::cout << "mRec.temp: " << expf(temp.g());
-        mRec.transmittance = Color3f(expf(temp.r()), expf(temp.g()), expf(temp.b()));
-        mRec.sigmaA = m_sigmaA;
-        mRec.sigmaS = m_sigmaS;
-
-        mRec.medium = this;
-        // if (mRec.transmittance.maxCoeff() < 1e-20)
-        //     std::cout << "pRec.pdf: " << mRec.transmittance;
-        //     mRec.transmittance = Color3f(0.0f);
-
-    }
-
-    /**
-     * \brief Compute the transmittance along a ray segment
-     *
-     * Computes the transmittance along a ray segment
-     * [mint, maxt] associated with the ray. It is assumed
-     * that the ray has a normalized direction value.
-     */
-    Color3f evalTransmittance(const Ray3f &ray, const Point2f &sample) const {
-        float negLength = ray.mint - ray.maxt;
-        Color3f transmittance;
+    Color3f sample(const Ray3f &ray, const Point2f &sample, MediumInteraction &mRec) const {
         
-        for (int i=0; i<3; ++i){
-            if(m_sigmaT[i] != 0 && negLength != 0) {
-                transmittance[i] = std::exp(m_sigmaT[i] * negLength);
-            } else {
-                cout << "transmittance: ";
-                //cout << "transmittance: " << medium[0]->evalTransmittance(ray, sampler->next1D());
-                transmittance[i] = 1.0f;
-            }
-        }
-        return transmittance;
+        // Sample one of the three channels and distance along the ray
+        int channel = std::min(int(sample.x() * 3), 3 - 1);
+        float dist = -std::log(1 - sample.y()) / m_sigmaT(channel);
+        float t = std::min(dist * ray.d.norm(), ray.maxt);
+        bool sampleMediumSuccess = t < ray.maxt;
+        if (sampleMediumSuccess)
+            mRec = MediumInteraction(ray(t), t, -(ray.d).normalized(), this->getPhaseFunction(), this);
+
+        // Compute the trasmittance and sampling density
+        Color3f Tr = exp(- m_sigmaT * std::min(t, INFINITY) * ray.d.norm()); //INFINITY OR MAXFLOAT
+            
+        // Return weighting factor for scattering from homogeneous medium
+        Color3f density = sampleMediumSuccess ? (m_sigmaT * Tr) : Tr;
+        float pdf = 0;
+        for (int i = 0; i < 3; i++)
+            pdf += density(i);
+        pdf *= 1.f / 3;
+        
+        if (sampleMediumSuccess) Tr *= m_sigmaS;
+
+        return Tr / pdf;        
     }
 
-    /// Determine whether the medium is homogeneous
-    bool isHomogeneous() const  {
-        return true;
-    }
-
-
-    /// Add a child
-    void addChild(NoriObject *obj, const std::string& name) {
-        switch (obj->getClassType()) {
-            case EPhaseFunction:
-                if (m_phaseFunction)
-                    throw NoriException(
-                        "Medium: tried to register multiple Phase functions!");
-                m_phaseFunction = static_cast<PhaseFunction *>(obj);
-                break;
-
-            default:
-                throw NoriException("Medium::addChild(<%s>) is not supported!",
-                                    classTypeName(obj->getClassType()));
-        }
+    /**
+     * \brief Compute the transmittance along a ray
+     *
+     */
+    Color3f Tr(const Ray3f &ray) const {
+        float d = std::min(ray.maxt, INFINITY); // MAXFLOAT or INFINITY
+        return exp(- m_sigmaT * d);
     }
 
     /// Return a string representation
@@ -157,6 +60,9 @@ public:
                     m_sigmaT,
                     m_albedo);
     }
+
+private:
+    Color3f m_sigmaA, m_sigmaS, m_sigmaT, m_albedo;
 };
 
 NORI_REGISTER_CLASS(HomogeneousMedium, "homogeneous");
