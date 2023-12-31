c81e447149540f6bf0e518452fe7d68f41a73ef5
luicalrob <luiscalderonr00@gmail.com>
2023-12-31 20:24:12 +0100

    new node

--- AR/project/drone_trayectory.cpp
+++ AR/project/drone_trayectory.cpp
@@ -44,6 +44,8 @@
 #include <px4_msgs/msg/vehicle_control_mode.hpp>
 #include <px4_msgs/msg/vehicle_odometry.hpp>
 #include <px4_msgs/msg/vehicle_attitude_setpoint.hpp>
+#include <px4_msgs/msg/vehicle_thrust_setpoint.hpp>
+#include <px4_msgs/msg/vehicle_torque_setpoint.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <stdint.h>
 
@@ -60,6 +62,27 @@ using namespace px4_msgs::msg;
 
 using std::placeholders::_1;
 
+class PIDController {
+private:
+    double Kp;
+    double Ki;
+    double Kd;
+    double integral;
+    double previous_error;
+
+public:
+    PIDController(double kp, double ki, double kd)
+        : Kp(kp), Ki(ki), Kd(kd), integral(0.0), previous_error(0.0) {}
+
+    double update(double error, double dt) {
+        integral += error * dt;
+        double derivative = (error - previous_error) / dt;
+        double output = Kp * error + Ki * integral + Kd * derivative;
+        previous_error = error;
+        return output;
+    }
+};
+
 class DroneTrayectory : public rclcpp::Node
 {
 
@@ -70,6 +93,46 @@ std::vector<std::vector<float>> targets;
 std::ifstream inFile;
 int currentTarget = 0; //index with the next target to reach
 
+private:
+	rclcpp::TimerBase::SharedPtr timer_;
+    float x_drone_, y_drone_, z_drone_;
+    float roll_degrees_, pitch_degrees_, yaw_degrees_;
+
+    // Control PID parameters
+    double Kp;
+    double Ki;
+    double Kd;
+    double integral;
+    double previous_error;
+    float max_thrust_xy = 50.0;
+    float max_thrust_z = 50.0;
+    float max_torque_xy = 50.0;
+    float max_torque_z = 50.0;
+
+	rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
+	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
+	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
+    rclcpp::Publisher<VehicleAttitudeSetpoint>::SharedPtr vehicle_attitude_setpoint_publisher_;
+    rclcpp::Publisher<VehicleThrustSetpoint>::SharedPtr vehicle_thrust_setpoint_publisher_;
+    rclcpp::Publisher<VehicleTorqueSetpoint>::SharedPtr vehicle_torque_setpoint_publisher_;
+
+	std::atomic<uint64_t> timestamp_;   //!< common synced timestamped
+
+	uint64_t offboard_setpoint_counter_;   //!< counter for the number of setpoints sent
+
+	void publish_offboard_control_mode();
+	void publish_trajectory_setpoint();
+	void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
+    void positionCb(const px4_msgs::msg::VehicleOdometry::SharedPtr msg);
+    void positionControl(const float currentPos[3], const float desiredPos[3], float thrust[3]);
+    void angularControl(const float currentAngles[3], const float desiredAngles[3], float torque[3]);
+    void quaternionToEulerAngles(const float q[4], float& roll, float& pitch, float& yaw);
+    void eulerAnglesToQuaternion(float roll, float pitch, float yaw, float q[4]);
+    float degreesToRadians(float degrees);
+    float radiansToDegrees(float radians);
+
+    rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr subscription_;
+
 public:
 	DroneTrayectory(const std::string &targets_file) : Node("drone_trayectory")
 	{
@@ -78,18 +141,13 @@ public:
 		trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
 		vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
         vehicle_attitude_setpoint_publisher_ = this->create_publisher<VehicleAttitudeSetpoint>("/fmu/in/vehicle_attitude_setpoint", 10);
+        vehicle_thrust_setpoint_publisher_ = this->create_publisher<VehicleThrustSetpoint>("/fmu/in/vehicle_thrust_setpoint", 10);
+        vehicle_torque_setpoint_publisher_ = this->create_publisher<VehicleTorqueSetpoint>("/fmu/in/vehicle_torque_setpoint", 10);
         
         rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
 		auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
 		subscription_ = this->create_subscription<px4_msgs::msg::VehicleOdometry>("/fmu/out/vehicle_odometry", qos,
 		std::bind(&DroneTrayectory::positionCb, this, _1));
-        // subscription_ = this->create_subscription<px4_msgs::msg::VehicleOdometry>(
-        // "/fmu/out/vehicle_odometry", qos,
-        // [this](const px4_msgs::msg::VehicleOdometry::UniquePtr msg) {
-        //     RCLCPP_INFO(this->get_logger(), "Received VehicleOdometry message");
-        //     // Add additional debug prints if needed
-        //     //this->positionCb(msg);
-        // });
 
         this->declare_parameter("targets_file", targets_file);
         std::string targets_file_param = this->get_parameter("targets_file").as_string();
@@ -150,7 +208,7 @@ public:
 
 			// offboard_control_mode needs to be paired with trajectory_setpoint
 			publish_offboard_control_mode();
-			publish_trajectory_setpoint();
+            publish_trajectory_setpoint();
 
 			// stop the counter after reaching 11
 			if (offboard_setpoint_counter_ < 11) {
@@ -162,31 +220,6 @@ public:
 
 	void arm();
 	void disarm();
-
-private:
-	rclcpp::TimerBase::SharedPtr timer_;
-    float x_drone_, y_drone_, z_drone_;
-    float ex_, ey_, ez_;
-
-	rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
-	rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
-	rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
-    rclcpp::Publisher<VehicleAttitudeSetpoint>::SharedPtr vehicle_attitude_setpoint_publisher_;
-
-	std::atomic<uint64_t> timestamp_;   //!< common synced timestamped
-
-	uint64_t offboard_setpoint_counter_;   //!< counter for the number of setpoints sent
-
-	void publish_offboard_control_mode();
-	void publish_trajectory_setpoint();
-	void publish_vehicle_command(uint16_t command, float param1 = 0.0, float param2 = 0.0);
-    void positionCb(const px4_msgs::msg::VehicleOdometry::SharedPtr msg);
-    void quaternionToEulerAngles(const float q[4], float& roll, float& pitch, float& yaw);
-    void eulerAnglesToQuaternion(float roll, float pitch, float yaw, float q[4]);
-    float degreesToRadians(float degrees);
-    float radiansToDegrees(float radians);
-
-    rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr subscription_;
 };
 
 /**
@@ -219,8 +252,9 @@ void DroneTrayectory::publish_offboard_control_mode()
 	msg.position = false;
 	msg.velocity = false;
 	msg.acceleration = false;
-	msg.attitude = true;
+	msg.attitude = false;
 	msg.body_rate = false;
+    msg.thrust_and_torque = true;
 	msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
 	offboard_control_mode_publisher_->publish(msg);
 }
@@ -232,42 +266,30 @@ void DroneTrayectory::publish_offboard_control_mode()
  */
 void DroneTrayectory::publish_trajectory_setpoint()
 {
-    // std::cout << "publish"  << std::endl;
-	TrajectorySetpoint msg_position{};
-    VehicleAttitudeSetpoint msg_angle{};
-	//msg.position = {0.0, 0.0, -5.0};
-	//msg.yaw = Goal[1][2]; // [-PI:PI]
+	VehicleThrustSetpoint msg_thrust{};
+    VehicleTorqueSetpoint msg_torque{};
 
-    // std::cout << "Drone Orientation: Roll=" << Goal[1][0]  << " degrees, "
-    // << "Pitch=" << Goal[1][1]  << " degrees, "
-    // << "Yaw=" << Goal[1][2]  << " degrees" << std::endl;
-        
-    msg_position.position = {Goal[0][0], Goal[0][1], Goal[0][2]};
-    //msg_position.yaw = Goal[1][2];
+    float currentPos[3] = {x_drone_, y_drone_, z_drone_};
+    float desiredPos[3] = {Goal[0][0], Goal[0][1], Goal[0][2]};
+    float thrust[3];
 
-    // msg_angle.roll_body = degreesToRadians(Goal[1][0]);
-    // msg_angle.pitch_body = degreesToRadians(Goal[1][1]);
-    // msg_angle.yaw_body = degreesToRadians(Goal[1][2]);
+    float currentAngles[3] = {roll_degrees_, pitch_degrees_, yaw_degrees_};
+    float desiredAngles[3] = {Goal[1][0], Goal[1][1], Goal[1][2]};
+    float torque[3];
 
-    //msg_angle.yaw_sp_move_rate = 0.2;
+    positionControl(currentPos, desiredPos, thrust);
+    angularControl(currentAngles, desiredAngles, torque);
 
-    msg_angle.thrust_body = {ex_/10.0, ey_/10.0, ez_/10.0};
-    // msg_angle.reset_integral =true;
-    float q[4];
-    eulerAnglesToQuaternion(Goal[1][0], Goal[1][1], Goal[1][2], q);
+    msg_thrust.xyz = {thrust[0], thrust[1], thrust[2]};
+    msg_torque.xyz = {torque[0], torque[1], torque[2]};
 
-    std::array<float, 4> q_array;
-    std::copy(std::begin(q), std::end(q), std::begin(q_array));
-
-    //msg_angle.q_d = q_array;
-
-    //msg.yaw = degreesToRadians(Goal[1][2]); // [-PI:PI]
     float time = this->get_clock()->now().nanoseconds() / 1000;
-	msg_position.timestamp = time;
-	msg_angle.timestamp = time;
-	//trajectory_setpoint_publisher_->publish(msg_position);
-    vehicle_attitude_setpoint_publisher_->publish(msg_angle);
 
+    msg_thrust.timestamp = time;
+	msg_torque.timestamp = time;
+
+    vehicle_thrust_setpoint_publisher_->publish(msg_thrust);
+    vehicle_torque_setpoint_publisher_->publish(msg_torque);
 }
 
 /**
@@ -295,6 +317,7 @@ void DroneTrayectory::positionCb(const px4_msgs::msg::VehicleOdometry::SharedPtr
     // RCLCPP_INFO(this->get_logger(), "Position X: %.2f, Y: %.2f and Z: %.2f",
     //             msg->position[0], msg->position[1], msg->position[2]);
     //std::cout << "GOT POSITION"  << std::endl;
+
     x_drone_ = msg->position[0];
     y_drone_ = msg->position[1];
     z_drone_ = msg->position[2];
@@ -306,41 +329,36 @@ void DroneTrayectory::positionCb(const px4_msgs::msg::VehicleOdometry::SharedPtr
 
     float roll, pitch, yaw;
     quaternionToEulerAngles(q, roll, pitch, yaw);
-    float roll_degrees, pitch_degrees, yaw_degrees;
-    roll_degrees = radiansToDegrees(roll);
-    pitch_degrees = radiansToDegrees(pitch);
-    yaw_degrees = radiansToDegrees(yaw);
 
-    std::cout << "goal desired x: " << Goal[0][0] << " y: " <<
-    Goal[0][1] << " z: " << Goal[0][2] << std::endl;
+    roll_degrees_ = radiansToDegrees(roll);
+    pitch_degrees_ = radiansToDegrees(pitch);
+    yaw_degrees_ = radiansToDegrees(yaw);
 
-    std::cout << "Drone Orientation: Roll=" << roll_degrees << " degrees, "
-        << "Pitch=" << pitch_degrees << " degrees, "
-        << "Yaw=" << yaw_degrees << " degrees" << std::endl;
+    // std::cout << "goal desired x: " << Goal[0][0] << " y: " <<
+    // Goal[0][1] << " z: " << Goal[0][2] << std::endl;
+
+    // std::cout << "Drone Orientation: Roll=" << roll_degrees_ << " degrees, "
+    //     << "Pitch=" << pitch_degrees_ << " degrees, "
+    //     << "Yaw=" << yaw_degrees_ << " degrees" << std::endl;
     
+    float ex = Goal[0][0] - x_drone_;
+	float ey = Goal[0][1] - y_drone_;
+	float ez = Goal[0][2] - z_drone_;
 
-    ex_ = Goal[0][0] - x_drone_;
-	ey_ = Goal[0][1] - y_drone_;
-	ez_ = Goal[0][2] - z_drone_;
+	float distance_to_target = sqrt(ex*ex+ey*ey+ez*ez);
 
-	float distance_to_target = sqrt(ex_*ex_+ey_*ey_+ez_*ez_);
-
-    float eRoll = roll_degrees - Goal[1][0];
-	float ePitch = pitch_degrees - Goal[1][1];
-	float eYaw = yaw_degrees - Goal[1][2];
+    float eRoll = Goal[1][0] - roll_degrees_;
+	float ePitch = Goal[1][1] - pitch_degrees_;
+	float eYaw = Goal[1][2] - yaw_degrees_;
 
 	float angle_to_target = sqrt(eRoll*eRoll+ePitch*ePitch+eYaw*eYaw);
 
+    // std::cout << "Roll desired: " << Goal[1][0] << " degrees, "
+    // << "Pitch desired: " << Goal[1][1] << " degrees, "
+    // << "Yaw desired:" << Goal[1][2] << " degrees" << std::endl;
 
-    
-    std::cout << "Roll desired: " << Goal[1][0] << " degrees, "
-    << "Pitch desired: " << Goal[1][1] << " degrees, "
-    << "Yaw desired:" << Goal[1][2] << " degrees" << std::endl;
-
-    std::cout << "distance to target: " << distance_to_target << std::endl;
-    std::cout << "angle to target: " << angle_to_target << std::endl;
-
-
+    // std::cout << "distance to target: " << distance_to_target << std::endl;
+    // std::cout << "angle to target: " << angle_to_target << std::endl;
 
     if(distance_to_target < minimum_distance && (angle_to_target < minimum_angle || (360 - angle_to_target) < minimum_angle)) {
         if (currentTarget == targets.size() - 1){
@@ -358,16 +376,50 @@ void DroneTrayectory::positionCb(const px4_msgs::msg::VehicleOdometry::SharedPtr
         Goal[1][0] = target.at(3);
         Goal[1][1] = target.at(4);
         Goal[1][2] = target.at(5);
+    }
+}
 
+void DroneTrayectory::positionControl(const float currentPos[3], const float desiredPos[3], float thrust[3]) {
+    static PIDController pidXY(0.95, 0.18, 0.04);  // Tune PID gains
+    static PIDController pidZ(1.0, 0.1, 0.02);
+    // static PIDController pidZ(1.0, 0.1, 0.01);
+    float error;
+    for (int i = 0; i < 2; ++i) {
+        error = desiredPos[i] - currentPos[i];
+        thrust[i] = pidXY.update(error, 0.1);
 
+        // Apply torque saturation and normalization
+        thrust[i] = std::min(std::max(thrust[i], -max_thrust_xy), max_thrust_xy) / max_thrust_xy;
     }
 
-    // TrajectorySetpoint msg_publish{};
-    // msg_publish.position = {Goal[0][0], Goal[0][1], Goal[0][2]};
-    // msg_publish.yaw = Goal[1][2]; // [-PI:PI]
-    // msg_publish.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-    // trajectory_setpoint_publisher_->publish(msg_publish);
+    error = desiredPos[2] - currentPos[2];
+    thrust[2] = pidZ.update(error, 0.1);
 
+    // Apply thrust saturation and normalization
+    thrust[2] = std::min(std::max(thrust[2], -max_thrust_z), max_thrust_z) / max_thrust_z;
+}
+
+void DroneTrayectory::angularControl(const float currentAngles[3], const float desiredAngles[3], float torque[3]) {
+    // static PIDController pidRoll(1.0, 0.1, 0.01);   // Tune PID gains
+    // static PIDController pidPitch(1.0, 0.1, 0.01);
+    // static PIDController pidYaw(1.0, 0.1, 0.01);
+    static PIDController pidXY(0.95, 0.18, 0.04);  // Tune PID gains
+    static PIDController pidZ(1.0, 0.1, 0.02);
+
+    float error;
+    for (int i = 0; i < 2; ++i) {
+        error = desiredAngles[i] - currentAngles[i];
+        torque[i] = pidXY.update(error, 0.1);  // Assuming a control loop frequency of 10 Hz
+
+        // Apply torque saturation and normalization
+        torque[i] = std::min(std::max(torque[i], -max_torque_xy), max_torque_xy) / max_torque_xy;
+    }
+
+    error = desiredAngles[2] - currentAngles[2];
+    torque[2] = pidZ.update(error, 0.1);
+
+    // Apply torque saturation and normalization
+    torque[2] = std::min(std::max(torque[2], -max_torque_z), max_torque_z) / max_torque_z;
 }
 
 void DroneTrayectory::quaternionToEulerAngles(const float q[4], float& roll, float& pitch, float& yaw)
