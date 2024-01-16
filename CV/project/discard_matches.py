1bec8ab2c0b8e7589fbb9abd069fffa39ef19bce
amartinezsilva <andresmarsil77@gmail.com>
2024-01-16 13:35:31 +0100

    discard_matches

--- CV/project/discard_matches.py
+++ CV/project/discard_matches.py
@@ -24,11 +24,11 @@ def visualize_2D_points(image, real_points, estimated_points):
 
     # Plot real points with red arrows
     ax.quiver(real_points[0], real_points[1], np.zeros_like(real_points[0]), np.zeros_like(real_points[1]),
-              angles='xy', scale_units='xy', scale=1, color='red', label='Kept Points')
+              angles='xy', scale_units='xy', scale=2, color='red', label='Discarded Points')
 
     # Plot estimated points with blue arrows
     ax.quiver(estimated_points[0], estimated_points[1], np.zeros_like(estimated_points[0]), np.zeros_like(estimated_points[1]),
-              angles='xy', scale_units='xy', scale=1, color='blue', label='Discarded Points')
+              angles='xy', scale_units='xy', scale=2, color='blue', label='Kept Points')
 
     # # Plot lines connecting corresponding points
     # for real_point, estimated_point in zip(real_points.T, estimated_points.T):
@@ -40,9 +40,6 @@ def visualize_2D_points(image, real_points, estimated_points):
 
     # Add legend
     ax.legend()
-
-    # Show the plot
-    plt.show()
 
 
 def visualize_matches(img1, kp1, img2, kp2, matches):
@@ -67,69 +64,105 @@ if __name__ == '__main__':
     img2 = cv2.cvtColor(cv2.imread(path_image_2), cv2.COLOR_BGR2RGB)
     img3 = cv2.cvtColor(cv2.imread(path_image_3), cv2.COLOR_BGR2RGB)
 
-    print("Number of matches with the old: ")
-    print(x_o1_1.shape[1])
+    print("Number of matches with the parallax: ")
+    print(x_p1_1.shape[1])
 
     threshold_error = 5
-    index_list_kept = []
+    index_list_kept_p = []
+    index_list_kept_o = []
 
     ##Select points
 
+    # for i in range(x_p1_1.shape[1]):
+    #     initial_error_x = x_p1_1[0,i] - x_o1_1[0,0]
+    #     initial_error_y = x_p1_1[1,i] - x_o1_1[1,0]
+    #     min_error = np.sqrt(initial_error_x * initial_error_x + initial_error_y * initial_error_y)
+    #     index_min_error = 0
+    #     for j in range(x_o1_1.shape[1]):
+    #         error_x = x_p1_1[0,i] - x_o1_1[0,j]
+    #         error_y = x_p1_1[1,i] - x_o1_1[1,j]
+    #         mse = np.sqrt(error_x * error_x + error_y * error_y)
+    #         if mse < min_error:
+    #             min_error = mse
+    #             index_min_error = j
+    #     if min_error < threshold_error:
+    #         print(min_error)
+    #         index_list_kept.append(index_min_error)
+
+    print("Number of matches with the old: ")
+    print(x_o1_1.shape[1])
+
     for i in range(x_o1_1.shape[1]):
+        initial_error_x = x_o1_1[0,i] - x_p1_1[0,0]
+        initial_error_y = x_o1_1[1,i] - x_p1_1[1,0]
+        min_error = np.sqrt(initial_error_x * initial_error_x + initial_error_y * initial_error_y)
+        index_min_error = 0
         for j in range(x_p1_1.shape[1]):
             error_x = x_o1_1[0,i] - x_p1_1[0,j]
             error_y = x_o1_1[1,i] - x_p1_1[1,j]
+            mse = np.sqrt(error_x * error_x + error_y * error_y)
+            if mse < min_error:
+                min_error = mse
+                index_min_error = j
+        if min_error < threshold_error:
+            index_list_kept_o.append(i)
+            index_list_kept_p.append(index_min_error)
 
-            if np.sqrt(error_x * error_x + error_y * error_y) < threshold_error:
-                index_list_kept.append(i)
+    index_list_discarded_o = [i for i in range(x_o1_1.shape[1]) if i not in index_list_kept_o]
+    index_list_discarded_p = [i for i in range(x_p1_1.shape[1]) if i not in index_list_kept_p]
 
-    index_list_discarded = [i for i in range(x_o1_1.shape[1]) if i not in index_list_kept]
+    print("P: Number of final matches: ")
+    print(len(index_list_kept_p))
+    print("P: Number of discarded matches: ")
+    print(len(index_list_discarded_p))
 
-    print("Number of final matches: ")
-    print(len(index_list_kept))
-    print("Number of discarded matches: ")
-    print(len(index_list_discarded))
+    print("O: Number of final matches: ")
+    print(len(index_list_kept_o))
+    print("O: Number of discarded matches: ")
+    print(len(index_list_discarded_o))
 
-    x_p1_1_kept = np.zeros((2,np.array(index_list_kept).shape[0]))
-    x_o1_1_kept = np.zeros((2,np.array(index_list_kept).shape[0]))
+    x_p1_1_kept = np.zeros((2,np.array(index_list_kept_p).shape[0]))
+    x_o1_1_kept = np.zeros((2,np.array(index_list_kept_o).shape[0]))
 
-    for i in range(np.array(index_list_kept).shape[0]): # [1 2 3 6 7 8 10 12]
-        x_p1_1_kept[:,i] = x_p1_1[:, index_list_kept[i]]
-        x_o1_1_kept[:,i] = x_o1_1[:, index_list_kept[i]]
+    for i in range(np.array(index_list_kept_p).shape[0]): # [1 2 3 6 7 8 10 12]
+        # x_p1_1_kept[:,i] = x_p1_1[:, index_list_kept[i]]
+        x_p1_1_kept[:,i] = x_p1_1[:, index_list_kept_p[i]]
 
-    # np.savetxt('x_p1_1_kept.txt', x_p1_1_kept, fmt='%1.8e', delimiter=' ')
-    # np.savetxt('x_o1_o_kept.txt', x_o1_o_kept, fmt='%1.8e', delimiter=' ')
-    # np.savetxt('x_p1_p_kept.txt', x_p1_p_kept, fmt='%1.8e', delimiter=' ')
+    for i in range(np.array(index_list_kept_o).shape[0]): # [1 2 3 6 7 8 10 12]
+        # x_p1_1_kept[:,i] = x_p1_1[:, index_list_kept[i]]
+        x_o1_1_kept[:,i] = x_o1_1[:, index_list_kept_o[i]]
 
-    x_p1_1_discarded = np.zeros((2,np.array(index_list_discarded).shape[0]))
-    x_o1_1_discarded = np.zeros((2,np.array(index_list_discarded).shape[0]))
 
-    for i in range(np.array(index_list_discarded).shape[0]): # [1 2 3 6 7 8 10 12]
-        x_p1_1_discarded[:,i] = x_p1_1[:, index_list_discarded[i]]
-        x_o1_1_discarded[:,i] = x_o1_1[:, index_list_discarded[i]]
+    x_p1_1_discarded = np.zeros((2,np.array(index_list_discarded_p).shape[0]))
+    x_o1_1_discarded = np.zeros((2,np.array(index_list_discarded_o).shape[0]))
 
-    visualize_2D_points(img1, x_o1_1_kept, x_o1_1_discarded)
-    # visualize_2D_points(img2, x_p1_p_kept, x_p1_p_discarded)
+    for i in range(np.array(index_list_discarded_o).shape[0]): # [1 2 3 6 7 8 10 12]
+        #x_p1_1_discarded[:,i] = x_p1_1[:, index_list_discarded[i]]
+        x_o1_1_discarded[:,i] = x_o1_1[:, index_list_discarded_o[i]]
+
+    
+    for i in range(np.array(index_list_discarded_p).shape[0]): # [1 2 3 6 7 8 10 12]
+        #x_p1_1_discarded[:,i] = x_p1_1[:, index_list_discarded[i]]
+        x_p1_1_discarded[:,i] = x_p1_1[:, index_list_discarded_p[i]]
+
+    visualize_2D_points(img1, x_p1_1_discarded, x_p1_1_kept)
+    visualize_2D_points(img1, x_o1_1_discarded, x_o1_1_kept)
     # visualize_2D_points(img3, x_o1_o_kept, x_o1_o_discarded)
     plt.show()
 
-    # #Visualize matches
-    # common_matches = []
-    # for idx in index_list:
-    #             match = cv2.DMatch(_queryIdx=idx, _trainIdx=idx, _distance=0)
-    #             common_matches.append(match)
 
-    # ##Unselected point
+    ###### Save same matches for three images ########
+    x_p1_p_kept = np.zeros((2,np.array(index_list_kept_p).shape[0]))
+    x_o1_o_kept = np.zeros((2,np.array(index_list_kept_o).shape[0]))
 
-    # discarded_index_list = []
-    # for j in range(x_p1_1.shape[1]):
-    #     discarded = True
-    #     for i in index_list:
-    #         if i == j: 
-    #             discarded = False
-    #             break
-    #     if discarded==True: discarded_index_list.append(j)
+    for i in range(np.array(index_list_kept_p).shape[0]): # [1 2 3 6 7 8 10 12]
+        # x_p1_1_kept[:,i] = x_p1_1[:, index_list_kept[i]]
+        x_p1_p_kept[:,i] = x_p1_p[:, index_list_kept_p[i]]
 
+    for i in range(np.array(index_list_kept_o).shape[0]): # [1 2 3 6 7 8 10 12]
+        # x_p1_1_kept[:,i] = x_p1_1[:, index_list_kept[i]]
+        x_o1_o_kept[:,i] = x_o1_o[:, index_list_kept_o[i]]
 
-    # print("Number of discarded matches: ")
-    # print(len(discarded_index_list))
+    np.savetxt('x_p1_1_kept.txt', x_p1_1_kept, fmt='%1.8e', delimiter=' ')
+    np.savetxt('x_o1_o_kept.txt', x_o1_o_kept, fmt='%1.8e', delimiter=' ')
+    np.savetxt('x_p1_p_kept.txt', x_p1_p_kept, fmt='%1.8e', delimiter=' ')
